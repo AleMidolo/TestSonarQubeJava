d9fdea5630ffa72c02ca2e794d05dd7d2ec67f55
AleMidolo <alemidolo@gmail.com>
2025-02-26 11:01:13 +0100

    Removed all files

--- src/main/java/org/636766ae1a6d9265ec0175d8/636766ae1a6d9265ec0175d8.java
+++
@@ -1,8 +0,0 @@
-private String buildContentRange() {
-    // Example values for demonstration purposes
-    long start = 0;
-    long end = 99;
-    long totalSize = 1000;
-
-    return String.format("bytes %d-%d/%d", start, end, totalSize);
-}

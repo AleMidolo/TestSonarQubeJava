7bf26b9095c00c89a57dda101aeec86ab1316c0b
AleMidolo <alemidolo@gmail.com>
2025-03-05 09:19:38 +0100

    Updated to skip parsing errors

--- src/main/java/org/636766fe1a6d9265ec017821/636766fe1a6d9265ec017821.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class ToStringBuilder {
-
-    protected void appendDetail(StringBuffer buffer, String fieldName, byte[] array) {
-        buffer.append(Arrays.toString(array));
-    }
-
-}
